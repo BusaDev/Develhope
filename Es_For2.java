20a41c764ad080bf074c1c94dd7aebe3ed1d3dfc
Davide Busà <busadavide95@gmail.com>
2024-02-23 17:10:47 +0100

    aggiunta esercizio array migliore

--- Es_For2.java
+++ Es_For2.java
@@ -1,13 +1,13 @@
 public class Es_For2 {
     public static void main(String args[]) {
-        int number = 5;
+        int number = 10;
         int limite = 3;
         stampa(number, limite);
     }
     public static void stampa (int number, int limite){
-        for(int i = 0; i < limite; i++){
-            System.out.println(number);
-            number --;
+        for(int i = number; i > limite; i--){
+            System.out.println(i);
+            
         }
     }
 }
