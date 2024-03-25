20d3ef13ed220bde69575072ed2bcd00934cf56e
Davide Busà <busadavide95@gmail.com>
2024-03-25 16:26:55 +0100

    esercizio Date2

--- src/Java_Advance/esDate2/Main.java
+++ src/Java_Advance/esDate2/Main.java
@@ -2,14 +2,13 @@ package Java_Advance.esDate2;
 
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
+
+import java.util.Locale;
 
 public class Main {
     public static void main(String args[]){
         OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-
-
-        System.out.println(data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)));
+        System.out.println(data.format(DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ITALY)));
 
     }
 }
