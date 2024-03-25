b2e8edcdfbf44fb0b4dddd8751cb9b2eb3a7fc07
Davide Busà <busadavide95@gmail.com>
2024-03-25 16:29:57 +0100

    esercizio Date3

---
+++ src/Java_Advance/esDate3/Main.java
@@ -0,0 +1,14 @@
+package Java_Advance.esDate3;
+
+import java.time.OffsetDateTime;
+
+
+public class Main {
+    public static void main(String args[]){
+        OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        System.out.println(data.getYear());
+        System.out.println(data.getMonth());
+        System.out.println(data.getDayOfMonth());
+        System.out.println(data.getDayOfWeek());
+    }
+}
