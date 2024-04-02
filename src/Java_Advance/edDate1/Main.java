addcfd042d40a48502600501b3d932c3463d15c7
Davide Busà <busadavide95@gmail.com>
2024-04-02 15:52:16 +0200

    esercizio Operatore Ternario 1

--- src/Java_Advance/edDate1/Main.java
+++ src/Java_Advance/edDate1/Main.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         ZonedDateTime data = OffsetDateTime.parse("2002-03-01T13:00:00Z").toZonedDateTime();
         System.out.println(data);
         System.out.println(data.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL)));
